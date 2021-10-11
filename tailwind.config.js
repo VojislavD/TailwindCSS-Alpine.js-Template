8909fa61037378e2f7d5b6ffc94ee6f8bdd7c427
VojislavD <vojislavdragicevic17@gmail.com>
2021-10-11 12:16:13 +0200

    Setup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./*'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#1f4b8e",
+        "primary-dark": "#102a52",
+        "secondary": "#182430",
+      }
+    },
   },
   variants: {
     extend: {},
